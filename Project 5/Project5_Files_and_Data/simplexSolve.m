f2d2af41144e29c7c525808297e478a78c02d619
Harkeerat Singh Sawhney <harkeerat@gmail.com>
2023-12-29 18:48:52 +0100

    Remove unnecessary files and functions & Finished Project 5

--- Project 5/Project5_Files_and_Data/simplexSolve.m
+++ Project 5/Project5_Files_and_Data/simplexSolve.m
@@ -8,18 +8,15 @@ nIter = 0;
 BiD = B\D;
 Bih = B\h;
 
-% TODO: Compute the reduced cost coefficients
-%r_D = 
+% TODO: Compute the reduced cost coefficients: Using the formula r_D = c_D - c_B * B\D
 r_D = c_D - (c_B * (B \ D));
 
 tol = max(size(r_D)); % the optimality condition is satisfied if all reduced cost coefficients are positive or negative (depending on the problem)
 
 % TODO: Check the optimality condition, in order to skip the loop if the solution is already optimal)
 if(strcmp(type,'max'))
-    %optCheck = 
     optCheck = sum(r_D <= 0);
 elseif(strcmp(type,'min'))
-    %optCheck = 
     optCheck = sum(r_D >= 0);
 else
     error('Incorrect type specified. Choose either a maximisation (max) or minimisation (min) problem.')
@@ -29,11 +26,9 @@ while(optCheck~=tol)
 
     % TODO: Find the index of the entering variable
     if(strcmp(type,'max'))
-        %idxIN = 
         [~, idx] = max(r_D);
         idxIN = idx(1);
     elseif(strcmp(type,'min'))
-        %idxIN = 
         [~, idx] = min(r_D);
         idxIN = idx(1);
     else
@@ -46,11 +41,9 @@ while(optCheck~=tol)
     index_in = index_D(1,idxIN);
     
     % TODO: Evaluate the coefficients ratio for the column corresponding to the entering variable
-    %ratio = 
     ratio = Bih ./ (inv(B) * in);
     
     % TODO: Find the smallest positive ratio
-    %idxOUT = 
 
     ratio(ratio < 0) = inf;
     [~, idxOUT] = min(ratio);
@@ -60,13 +53,6 @@ while(optCheck~=tol)
     index_out = index_B(1,idxOUT);
     
     % TODO: Update the matrices by exchanging the columns
-    %B(:,idxOUT) =
-    %D(:,idxIN) =
-    %c_B(1,idxOUT) =
-    %c_D(1,idxIN) =
-    %index_B(1,idxOUT) =
-    %index_D(1,idxIN) =
-
     B(:,idxOUT) = in;
     D(:,idxIN) = out;
     c_B(1,idxOUT) = c_in;
@@ -79,7 +65,6 @@ while(optCheck~=tol)
     Bih = B\h;
     
     % TODO: Compute the reduced cost coefficients
-    %r_D = 
     r_D = c_D - (c_B * (B \ D));
 
     % TODO: Check the optimality condition 
@@ -100,7 +85,6 @@ while(optCheck~=tol)
     end
 
     % TODO: Compute the new x_B
-    %x_B = 
     x_B = Bih - (BiD * x_D);
 
 end
